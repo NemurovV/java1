0d3eca6ea9ab7dc328819973671897a944744ea0
NemurovV <nemurovv@rambler.ru>
2020-05-31 20:42:47 +1000

    H4 Test restart modified methods in classes TriangleInfo version three in package compare_if_cycles 31.05.2020

--- src/ru/progwards/java1/lessons/compare_if_cycles/TriangleSimpleInfo.java
+++ src/ru/progwards/java1/lessons/compare_if_cycles/TriangleSimpleInfo.java
@@ -2,52 +2,54 @@ package ru.progwards.java1.lessons.compare_if_cycles;
 
 public class TriangleSimpleInfo {
     public static void main(String[] args) {
-        System.out.println(maxSide(10, 15,21));
-        System.out.println(minSide(2, 10, 0));
+        System.out.println(maxSide(10, 8, 10));
+        System.out.println(minSide(3, 4, 3));
         System.out.println(isEquilateralTriangle(2, 2, 1));
     }
-/*1.1 Реализовать функцию
-public static int maxSide(int a, int b, int c), которая получает параметрами длины сторон треугольника,
-а вернуть должна наибольшую длину стороны.
-*/
-public static int maxSide(int a, int b, int c) {
-    int max;
-    if (a > b && a > c) {
-        max = a;
-    }else if (b > a && b > c) {
-        max = b;
-    } else {
-        max = c;
-    }
-    return max;
-}
 
-/*
-1.2 Реализовать функцию
-public static int minSide(int a, int b, int c), которая получает параметрами длины сторон треугольника,
-а вернуть должна наименьшую длину стороны.
-*/
-public static int minSide(int a, int b, int c) {
-    int min;
-    if (a < b && a < c) {
-        min = a;
-    }else if (b < a && b < c) {
-        min = b;
-    } else {
-        min = c;
+    /*1.1 Реализовать функцию
+    public static int maxSide(int a, int b, int c), которая получает параметрами длины сторон треугольника,
+    а вернуть должна наибольшую длину стороны.
+    */
+    public static int maxSide(int a, int b, int c) {
+        int max = 0;
+        if (a >= b && a >= c) {
+            max = a;
+        } else if (b >= a && b >= c) {
+            max = b;
+        } else {
+            max = c;
+        }
+        return max;
     }
-    return min;
 
-}
-/*
-1.3 Реализовать функцию
-public static boolean isEquilateralTriangle(int a, int b, int c), которая получает параметрами длины сторон треугольника,
-а вернуть должна true, если треугольник равносторонний и false в противном случае.
-*/
-public static boolean isEquilateralTriangle(int a, int b, int c) {
-    boolean bool;
-    return  a == b && a == c;
-}
+    /*
+    1.2 Реализовать функцию
+    public static int minSide(int a, int b, int c), которая получает параметрами длины сторон треугольника,
+    а вернуть должна наименьшую длину стороны.
+    */
+    public static int minSide(int a, int b, int c) {
+        int min = 0;
+        if (a <= b && a <= c) {
+            min = a;
+        } else if (b <= a && b <= c) {
+            min = b;
+        } else {
+            min = c;
+        }
+        return min;
+
+    }
+
+    /*
+    1.3 Реализовать функцию
+    public static boolean isEquilateralTriangle(int a, int b, int c), которая получает параметрами длины сторон треугольника,
+    а вернуть должна true, если треугольник равносторонний и false в противном случае.
+    */
+    public static boolean isEquilateralTriangle(int a, int b, int c) {
+        boolean bool;
+        return a == b && a == c;
+    }
 
 }
 
